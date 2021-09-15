af411fcd9b0f6f8b6b6f54cebf54778149782610
StanDenisov <go.prog.freelance@hotmail.com>
2021-09-15 14:23:33 +0300

    improve bags filds

--- confstruct/confstruct.go
+++ confstruct/confstruct.go
@@ -2,10 +2,11 @@ package confstruct
 
 //ConfStruct - structer for configurating microservices
 type ConfStruct struct {
-	AppMode       string `json:"app_mode,omitempty"`
-	AppName       string `json:"app_name,omitempty"`
-	PgDBPort      string `json:"pg_db_port,omitempty"`
-	PgDBPassword  string `json:"pg_db_password,omitempty"`
-	PgDBUsernname string `json:"pg_db_usernname,omitempty"`
-	PgDBName      string `json:"pg_db_name,omitempty"`
+	AppPort      string `json:"app_port,omitempty"`
+	AppMode      string `json:"app_mode,omitempty"`
+	AppName      string `json:"app_name,omitempty"`
+	PgDBPort     string `json:"pg_db_port,omitempty"`
+	PgDBPassword string `json:"pg_db_password,omitempty"`
+	PgDBUserName string `json:"pg_db_user_name,omitempty"`
+	PgDBName     string `json:"pg_db_name,omitempty"`
 }
